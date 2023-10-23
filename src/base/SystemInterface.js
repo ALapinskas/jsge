132bee029685270dbdaf55d273c17892ceba5a0b
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2023-10-23 10:03:31 +0300

    0.2.11: fixed dependency issue

--- src/base/SystemInterface.js
+++ src/base/SystemInterface.js
@@ -3,7 +3,7 @@ import { Exception, Warning } from "./Exception.js";
 import { SystemSocketConnection } from "./SystemSocketConnection.js";
 import { SystemAudioInterface } from "./SystemAudioInterface.js";
 import { SystemSettings } from "../configs.js";
-import AssetsManager from "../../node_modules/assetsm/dist/assetsm.min.js";
+import AssetsManager from "../../modules/assetsm/dist/assetsm.min.js";
 import { DrawImageObject } from "./DrawImageObject.js";
 import { DrawObjectFactory } from "./DrawObjectFactory.js";
 
