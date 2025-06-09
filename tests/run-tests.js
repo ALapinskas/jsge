940442686e7f8c1ce813d1e09c14ac764bc41501
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2025-06-09 15:54:43 +0300

    fixed run-tests

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -24,20 +24,20 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
     testProcess.stderr.on('data', (data) => {
         console.error(`Test Error: ${data}`);
-        server.kill();
+        server.kill('SIGQUIT');
     });
 
     testProcess.on('exit', (code) => {
-        console.log(`Test script exited with code ${code}`);
-        server.kill();
+        console.log(`Puppeteer exited with code ${code}`);
+        server.kill('SIGQUIT');
         // Determine the command to terminate Node.js processes based on the OS
         const command = process.platform === 'win32' ? 'taskkill /f /im node.exe' : 'pkill node';
 
         exec(command, (err, stdout, stderr) => {
         if (err) {
-            console.error(`Error terminating Node.js processes: ${stderr}`);
+            console.error(`Error terminating Puppeteer processes: ${stderr}`);
         } else {
-            console.log(`Terminated Node.js processes: ${stdout}`);
+            console.log(`Terminated Puppeteer processes: ${stdout}`);
         }
         });
     });
