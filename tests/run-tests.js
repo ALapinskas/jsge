805e3f989914a5a78f094629e9de7f4ef41116c0
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2025-06-09 15:46:21 +0300

    moved server to child-process, added shutdown

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -1,36 +1,44 @@
 // run-tests.js
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 // Function to sleep for a specified duration
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 (async () => {
-  // Sleep for 3 seconds
-  await sleep(3000);
-
-  // Start the regression tests
-  const testProcess = exec('node ./tests/regression.js');
-
-  testProcess.stdout.on('data', (data) => {
-    console.log(`Test Output: ${data}`);
-  });
-
-  testProcess.stderr.on('data', (data) => {
-    console.error(`Test Error: ${data}`);
-  });
-
-  testProcess.on('exit', (code) => {
-    console.log(`Test script exited with code ${code}`);
     
-    // Determine the command to terminate Node.js processes based on the OS
-    const command = process.platform === 'win32' ? 'taskkill /f /im node.exe' : 'pkill node';
-
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error terminating Node.js processes: ${stderr}`);
-      } else {
-        console.log(`Terminated Node.js processes: ${stdout}`);
-      }
+    const server = spawn('http-server', ["./", '-c-1', '-p', '9000'], {
+        stdio: 'inherit', // Inherit stdio to see output in the console
+        shell: true // Use shell to execute the command
     });
-  });
+
+    // Sleep for 3 seconds
+    await sleep(3000);
+
+    // Start the regression tests
+    const testProcess = exec('node ./tests/regression.js');
+    
+
+    testProcess.stdout.on('data', (data) => {
+        console.log(`Test Output: ${data}`);
+    });
+
+    testProcess.stderr.on('data', (data) => {
+        console.error(`Test Error: ${data}`);
+        server.kill();
+    });
+
+    testProcess.on('exit', (code) => {
+        console.log(`Test script exited with code ${code}`);
+        server.kill();
+        // Determine the command to terminate Node.js processes based on the OS
+        const command = process.platform === 'win32' ? 'taskkill /f /im node.exe' : 'pkill node';
+
+        exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.error(`Error terminating Node.js processes: ${stderr}`);
+        } else {
+            console.log(`Terminated Node.js processes: ${stdout}`);
+        }
+        });
+    });
 })();
