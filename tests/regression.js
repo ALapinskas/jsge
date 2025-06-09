1bf067defc61c63af72cd1511b89da8198104478
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2025-06-09 14:40:23 +0300

    add no-sandbox option

--- tests/regression.js
+++ tests/regression.js
@@ -4,7 +4,7 @@ const TIMEOUT = 1;
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({/*executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",*/ headless: false});
+    const browser = await puppeteer.launch({/*executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",*/ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
   
     const CHECKED = 1,
