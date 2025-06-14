3adc220dba4a56e65ce1e39e0d410def74a0eb90
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2025-06-14 11:27:48 +0300

    added headless prop

--- tests/regression.js
+++ tests/regression.js
@@ -4,7 +4,7 @@ const TIMEOUT = 20;
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({/*executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",*/ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu']});
+    const browser = await puppeteer.launch({/*executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",*/ headless: "shell", args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu']});
     const page = await browser.newPage();
   
     const CHECKED = 1,
